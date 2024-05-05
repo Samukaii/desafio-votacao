2dc88f9f5586d21f8f7d1c5345faff758546a2a8
samuel <samuel.veras@joinin.com.br>
2024-05-05 03:32:11 -0300

    fix(associate-dto): Adicionada validação de tamanho

--- api/src/main/java/com/dbserver/votingchallenge/dtos/associated/AssociatedCreateDTO.java
+++ api/src/main/java/com/dbserver/votingchallenge/dtos/associated/AssociatedCreateDTO.java
@@ -1,5 +1,6 @@
 package com.dbserver.votingchallenge.dtos.associated;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
@@ -8,6 +9,7 @@ public record AssociatedCreateDTO(
         @NotBlank
         String name,
         @NotBlank
+        @Digits(integer = 11, fraction = 0)
         String cpf
 ) {
 }
